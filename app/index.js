a2e56bd694842c57025a01941deafeaaf0d2f09d
Yoshiyuki Hirano <yhirano@aiming-inc.com>
2017-02-28 05:20:05 +0900

    app init

---
+++ app/index.js
@@ -0,0 +1,7 @@
+function component() {
+  var elem = document.createElement("div");
+  elem.innerHTML = _.join(["hello", "webpack"], ", ");
+  return elem;
+}
+
+document.body.appendChild(component());
