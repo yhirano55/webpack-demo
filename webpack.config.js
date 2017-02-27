111faeb51c82113703219bf8598ba4e3c005e3ac
Yoshiyuki Hirano <yhirano@aiming-inc.com>
2017-02-28 05:55:05 +0900

    modify webpack.config.js to add loaders

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,15 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/, loaders: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.jpg$/, loaders: ["url-loader"]
+      }
+    ]
   }
 };
